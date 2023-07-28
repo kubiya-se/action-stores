da488ac311beb5bfd545aecf45b546ad1dd23d52
michagonzo77 <michagonzo77@gmail.com>
2023-07-28 14:59:12 -0400

    debugging gitlab as

--- gitlab/main_store.py
+++ gitlab/main_store.py
@@ -5,7 +5,6 @@ action_store = kubiya.ActionStore("action-store-gitlab", "0.1.0")
 action_store.uses_secrets(["GITLAB_URL", "GITLAB_TOKEN"])
 
 from .actions import(
-    projects,
     groups,
     merge_requests,
     issues
